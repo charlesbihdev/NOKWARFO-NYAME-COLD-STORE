cec76fce574a4d7102ae4b9c90935da518bc75b4
Charles Owusu Bih <charlesowusubih@gmail.com>
2025-08-11 20:24:48 +0000

    ddd

--- resources/js/layouts/app/app-header-layout.jsx
+++ resources/js/layouts/app/app-header-layout.jsx
@@ -3,9 +3,10 @@ import { AppHeader } from '@/components/app-header';
 import { AppShell } from '@/components/app-shell';
 
 export default function AppHeaderLayout({ children, breadcrumbs }) {
-    return (<AppShell>
-            <AppHeader breadcrumbs={breadcrumbs}/>
+    return (
+        <AppShell>
+            <AppHeader breadcrumbs={breadcrumbs} />
             <AppContent>{children}</AppContent>
-        </AppShell>);
+        </AppShell>
+    );
 }
-
