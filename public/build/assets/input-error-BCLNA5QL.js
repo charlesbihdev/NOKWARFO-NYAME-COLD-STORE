d91c4dee73db03da317bcc0bc65bdd93dc215456
Charles Owusu Bih <charlesowusubih@gmail.com>
2025-08-13 06:20:32 +0000

    done build

---
+++ public/build/assets/input-error-BCLNA5QL.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-Cr2q6RgE.js";import{c as o}from"./createLucideIcon-BWJBnjvx.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
