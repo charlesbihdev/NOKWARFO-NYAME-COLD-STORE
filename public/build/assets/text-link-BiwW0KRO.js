0071cf91240fa6a06db113e2e91c70b1115004e2
Charrles Owusu Bih <charlesowusubih@gmail.com>
2025-09-02 20:13:47 +0000

    Update asset manifest and remove unused JavaScript and CSS files to optimize project structure; rename welcome.js file for consistency.

--- public/build/assets/text-link-BiwW0KRO.js
+++
@@ -1,1 +0,0 @@
-import{j as t,L as n}from"./app-Z_70yno1.js";import{b as a}from"./createLucideIcon-BvOV7x4M.js";function u({className:r="",children:e,...o}){return t.jsx(n,{className:a("text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500",r),...o,children:e})}export{u as T};
