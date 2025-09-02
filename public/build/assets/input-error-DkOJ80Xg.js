5d447753b729969a313117ed936586286b70cc28
Charrles Owusu Bih <charlesowusubih@gmail.com>
2025-09-02 19:34:40 +0000

    prod

---
+++ public/build/assets/input-error-DkOJ80Xg.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-Z_70yno1.js";import{b as o}from"./createLucideIcon-BvOV7x4M.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
