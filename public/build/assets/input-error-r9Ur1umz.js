cea6c7f3ee54924a2cb225d1f8a435453ab52d03
Charles Owusu Bih <charlesowusubih@gmail.com>
2026-04-05 06:07:31 +0000

    build after fix

---
+++ public/build/assets/input-error-r9Ur1umz.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-CVtN7IBi.js";import{a as o}from"./createLucideIcon-Cuvzmk0g.js";function a({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{a as I};
